20f34ab84c7dd27680a9b0d563f628aeca9bec2e
Isaac Hessel-Robinson <isaacHhyphenR@gmail.com>
2024-12-31 10:27:21 -0600

    all tests for GridService <Get>

--- P1_Battleship/P1_Battleship.TEST/GridServiceTests.cs
+++ P1_Battleship/P1_Battleship.TEST/GridServiceTests.cs
@@ -2,11 +2,15 @@ using Moq;
 using Battleship.API.Model;
 using Battleship.API.Repository;
 using Battleship.API.Service;
+using Battleship.API.GridException;
 
 namespace Battleship.TEST;
 
-public class UnitTest1
+public class GridServiceTests
 {
+///////////////////////////////////////////////////////////////////////////////
+///GET
+///////////////////////////////////////////////////////////////////////////////
     [Fact]
     public void GetAllGridsTest()
     {
@@ -38,4 +42,74 @@ public class UnitTest1
         //Assert
         Assert.Equal(gridList, result);
     }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    public void GetGridByIdTestValid(int _gridId)
+    {
+        //Arrange
+        Mock<IGridRepository> mockGridRepo = new();
+        Mock<IShipService> mockShipService = new();
+        GridService gridService = new(mockGridRepo.Object, mockShipService.Object);
+
+        List<Grid> gridList = [
+            new Grid{Id = 1,
+                columns = ["          ", "          ", "          ", "          ", "          ", "          ", "          ", "          ", "          ", "          "],
+                width = 10,
+                height = 10,
+                shipIds = [-1, -1, -1, -1, -1 ]
+            },
+            new Grid{Id = 2,
+                columns = ["  X       ", "   X      ", "     ---  ", "          ", "          "],
+                width = 5,
+                height = 10,
+                shipIds = [2, -1, -1, -1, 3 ]
+            }
+        ];
+
+        mockGridRepo.Setup(repo => repo.GetGridById(_gridId)).Returns(gridList[_gridId]);
+        
+        //Act
+        var result = gridService.GetGridById(_gridId);
+        
+        //Assert
+        Assert.Equal(gridList[_gridId], result);
+    }
+    [Theory]
+    [InlineData(-2)]
+    [InlineData(5)]
+    public void GetGridByIdTestInvalid(int _gridId)
+    {
+        //Arrange
+        Mock<IGridRepository> mockGridRepo = new();
+        Mock<IShipService> mockShipService = new();
+        GridService gridService = new(mockGridRepo.Object, mockShipService.Object);
+
+        List<Grid> gridList = [
+            new Grid{Id = 1,
+                columns = ["          ", "          ", "          ", "          ", "          ", "          ", "          ", "          ", "          ", "          "],
+                width = 10,
+                height = 10,
+                shipIds = [-1, -1, -1, -1, -1 ]
+            },
+            new Grid{Id = 2,
+                columns = ["  X       ", "   X      ", "     ---  ", "          ", "          "],
+                width = 5,
+                height = 10,
+                shipIds = [2, -1, -1, -1, 3 ]
+            }
+        ];
+
+        mockGridRepo.Setup(repo => repo.GetGridById(_gridId));
+        
+        //Act
+        //var result = gridService.GetGridById(_gridId);
+        
+        //Assert
+        Assert.Throws<GridUnknownException>(() => gridService.GetGridById(_gridId));
+    }
+///////////////////////////////////////////////////////////////////////////////
+///POST
+///////////////////////////////////////////////////////////////////////////////
 }
